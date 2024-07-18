6fa08069b3f22dff9870147f42f3428fd9b3893f
senullmd <gunarathnaanoja@gmail.com>
2024-07-18 17:45:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> Senul Radawa Md😽';
 global.owner = process.env.OWNER_NUMBER  || '94710773101' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=vAshCZJS#9sXQkVDwSr7VbILde5jpXqqsjbLm7SuT-em50XuwfU8' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rQlkgLyA#w51WHpi9Q4oIUm8jI_y2ZjniDyDnBNBKWvV2kxmwV0M' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
